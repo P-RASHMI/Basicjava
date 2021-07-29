4ae26118809abee724be58856c2d238829e365e4
P-RASHMI <imrisurashmi25@gmail.com>
2021-07-29 21:06:38 +0530

    .git file added

--- src/com/basicjava/Hello.java
+++ src/com/basicjava/Hello.java
@@ -2,10 +2,9 @@ package com.basicjava;
 
 public class Hello {
 	public static void main(String[] args) {
-		System.out.println("hello world!!! this is eclipse");
 	    System.out.println("hi this is rashmi");
 	    System.out.println("subject");
-	    System.out.println("branching");
+	    System.out.println("changed");
 	    
 	}
 }
