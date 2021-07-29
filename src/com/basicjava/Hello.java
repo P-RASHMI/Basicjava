2cb9a1ff1ad48ebed94b9958ab208157d0a2492a
P-RASHMI <imrisurashmi25@gmail.com>
2021-07-29 20:33:06 +0530

    second commit for first pgm

--- src/com/basicjava/Hello.java
+++ src/com/basicjava/Hello.java
@@ -3,5 +3,7 @@ package com.basicjava;
 public class Hello {
 	public static void main(String[] args) {
 		System.out.println("hello world!!! this is eclipse");
+	    System.out.println("hi this is rashmi");
+	    System.out.println("subject");
 	}
 }
